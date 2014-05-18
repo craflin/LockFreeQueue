f2f170d35eecabcac6333d6852cba9e22fc9ed96
Colin Graf <colin.graf@sovereign-labs.com>
2014-05-18 10:49:57 +0200

    Simplify naive queue implementation

--- NaiveQueue.h
+++ NaiveQueue.h
@@ -10,47 +10,32 @@ public:
   explicit NaiveQueue(size_t capacity) : _capacity(capacity)
   {
     queue = (Node*)Memory::alloc(sizeof(Node) * _capacity);
-    for(Node* node = queue, * end = queue + _capacity; node < end; ++node)
-      node->state = Node::free;
 
-    _occupiedNodes = 0;
-    _writeIndex = -1;
-    _readIndex = -1;
+    _head = 0;
+    _tail = 0;
   }
 
   ~NaiveQueue()
   {
-    for(Node* node = queue, * end = queue + _capacity; node < end; ++node)
-      switch(node->state)
-      {
-      case Node::set:
-      case Node::occupied:
-        (&node->data)->~T();
-        break;
-      default:
-        break;
-      }
+    for(size_t i = _head; i != _tail; ++i)
+      (&queue[i % _capacity].data)->~T();
     Memory::free(queue);
   }
   
   size_t capacity() const {return _capacity;}
   
-  size_t size() const {return _capacity - _freeNodes;}
+  size_t size() const {return _tail - _head;}
   
   bool_t push(const T& data)
   {
     mutex.lock();
-    if(_occupiedNodes == _capacity)
+    if(_tail - _head == _capacity)
     {
       mutex.unlock();
       return false; // queue is full
     }
-    ++_writeIndex;
-    Node& node = queue[_writeIndex % _capacity];
-    ASSERT(node.state == Node::free);
+    Node& node = queue[(_tail++) % _capacity];
     new (&node.data)T(data);
-    node.state =  Node::occupied;
-    ++_occupiedNodes;
     mutex.unlock();
     return true;
   }
@@ -58,18 +43,14 @@ public:
   bool_t pop(T& result)
   {
     mutex.lock();
-    if(_occupiedNodes == 0)
+    if(_head == _tail)
     {
       mutex.unlock();
       return false; // queue is empty
     }
-    --_occupiedNodes;
-    ++_readIndex;
-    Node& node = queue[_readIndex % _capacity];
-    ASSERT(node.state == Node::occupied);
+    Node& node = queue[(_head++) % _capacity];
     result = node.data;
     (&node.data)->~T();
-    node.state = Node::free;
     mutex.unlock();
     return true;
   }
@@ -78,21 +59,12 @@ private:
   struct Node
   {
     T data;
-    enum State
-    {
-      free,
-      set,
-      occupied,
-      unset,
-    };
-    volatile int_t state;
   };
 
 private:
   size_t _capacity;
   Node* queue;
-  size_t _occupiedNodes;
-  size_t _writeIndex;
-  size_t _readIndex;
+  size_t _head;
+  size_t _tail;
   Mutex mutex;
 };
