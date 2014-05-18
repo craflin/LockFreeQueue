ac35b454972b35b5e0910c4e9074a0b19f931637
Colin Graf <colin.graf@sovereign-labs.com>
2014-05-18 13:00:33 +0200

    Test all queue implementations in a row

--- Test.cpp
+++ Test.cpp
@@ -10,37 +10,62 @@
 #include "MutexLockQueue.h"
 #include "SpinLockQueue.h"
 
-#define TestQueue LockFreeQueue
-//#define TestQueue LockFreeQueueSlow1
-//#define TestQueue MutexLockQueue
-//#define TestQueue SpinLockQueue
+static const int testItems = 1000000 * 16;
+static const int testThreadConsumerThreads = 4;
+static const int testThreadProducerThreads = 4;
+static const int testItemsPerConsumerThread = testItems / testThreadConsumerThreads;
+static const int testItemsPerProducerThread = testItems / testThreadProducerThreads;
+
+template<typename T> class IQueue
+{
+public:
+  virtual size_t size() const = 0;
+  virtual bool_t push(const T& data) = 0;
+  virtual bool_t pop(T& result) = 0;
+};
+
+template<typename T, class Q> class TestQueue : public IQueue<T>
+{
+public:
+  TestQueue(size_t size) : queue(size) {}
+  size_t size() const {return queue.size();}
+  bool_t push(const T& data) {return queue.push(data);}
+  bool_t pop(T& result) {return queue.pop(result);}
+private:
+  Q queue;
+};
+
+volatile size_t producerSum;
+volatile size_t consumerSum;
 
 uint_t producerThread(void_t* param)
 {
-  TestQueue<int_t>* queue = (TestQueue<int_t>*)param;
-  for(int i = 0; i < 10000000; ++i)
+  IQueue<int_t>* queue = (IQueue<int_t>*)param;
+  for(int_t i = 0; i < testItemsPerProducerThread; ++i)
   {
-    while(!queue->push(12))
+    while(!queue->push(i))
       Thread::yield();
+    Atomic::fetchAndAdd(producerSum, i);
   }
   return 0;
 }
 
 uint_t consumerThread(void_t* param)
 {
-  TestQueue<int_t>* queue = (TestQueue<int_t>*)param;
+  IQueue<int_t>* queue = (IQueue<int_t>*)param;
   int_t val;
-  for(int i = 0; i < 10000000; ++i)
+  for(int_t i = 0; i < testItemsPerConsumerThread; ++i)
   {
     while(!queue->pop(val))
       Thread::yield();
+    Atomic::fetchAndAdd(consumerSum, val);
   }
   return 0;
 }
 
-int_t main(int_t argc, char_t* argv[])
+template<class Q> void_t testQueue(const String& name)
 {
-  Console::printf(_T("%s\n"), _T("Testing..."));
+  Console::printf(_T("Testing %s... "), (const tchar_t*)name);
 
   volatile int32_t int32 = 0;
   volatile uint32_t uint32 = 0;
@@ -57,7 +82,7 @@ int_t main(int_t argc, char_t* argv[])
   ASSERT(uint64 == 1);
 
   {
-    TestQueue<int_t> queue(10000);
+    TestQueue<int_t, Q> queue(10000);
     int_t result;
     ASSERT(!queue.pop(result));
     ASSERT(queue.push(42));
@@ -67,7 +92,7 @@ int_t main(int_t argc, char_t* argv[])
   }
 
   //{
-  //  LockFreeQueue<int_t> queue(0);
+  //  TestQueue<int_t, Q> queue(0);
   //  int_t result;
   //  ASSERT(!queue.pop(result));
   //  ASSERT(!queue.push(42));
@@ -76,7 +101,7 @@ int_t main(int_t argc, char_t* argv[])
   //}
 
   {
-    TestQueue<int_t> queue(2);
+    TestQueue<int_t, Q> queue(2);
     int_t result;
     ASSERT(!queue.pop(result));
     ASSERT(queue.push(42));
@@ -96,18 +121,20 @@ int_t main(int_t argc, char_t* argv[])
     ASSERT(!queue.push(48));
   }
 
+  producerSum = 0;
+  consumerSum = 0;
 
   timestamp_t startTime = Time::ticks();
   {
-    TestQueue<int_t> queue(10000);
+    TestQueue<int_t, Q> queue(10000);
     List<Thread*> threads;
-    for(int_t i = 0; i < 4; ++i)
+    for(int_t i = 0; i < testThreadProducerThreads; ++i)
     {
       Thread* thread = new Thread;
       thread->start(producerThread, &queue);
       threads.append(thread);
     }
-    for(int_t i = 0; i < 4; ++i)
+    for(int_t i = 0; i < testThreadConsumerThreads; ++i)
     {
       Thread* thread = new Thread;
       thread->start(consumerThread, &queue);
@@ -119,7 +146,18 @@ int_t main(int_t argc, char_t* argv[])
       thread->join();
       delete thread;
     }
+    ASSERT(queue.size() == 0);
   }
   timestamp_t duration = Time::ticks() - startTime;
-  Console::printf(_T("it took %lld ms\n"), duration);
+  Console::printf(_T("%lld ms, producerSum=%llu, consumerSum=%llu\n"), duration, (uint64_t)producerSum, (uint64_t)consumerSum);
+}
+
+int_t main(int_t argc, char_t* argv[])
+{
+  testQueue<LockFreeQueueSlow1<int_t> >("LockFreeQueueSlow1");
+  testQueue<LockFreeQueue<int_t> >("LockFreeQueue");
+  testQueue<MutexLockQueue<int_t> >("MutexLockQueue");
+  testQueue<SpinLockQueue<int_t> >("SpinLockQueue");
+
+  return 0;
 }
